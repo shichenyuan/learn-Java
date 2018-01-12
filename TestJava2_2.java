debf27c859111c7ab49b91df81d3cf9ef20e29b7
shichenyuan <34560365+shichenyuan@users.noreply.github.com>
2018-01-12 15:40:35 +0800

    Create TestJava2_2.java

---
+++ TestJava2_2.java
@@ -0,0 +1,12 @@
+//TestJava2_2,简单的Java程序
+public class TestJava2_2
+{
+  public static void main(String args[])
+  {
+    int x;
+    for(x=1;x<3;x++);
+    {
+      System.out.println(x+"*"+x+"="+x*x)
+    }
+  }
+}
