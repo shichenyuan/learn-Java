c61d480ec313dc6f941eb59832aad46ba0f6b777
shichenyuan <34560365+shichenyuan@users.noreply.github.com>
2018-01-12 15:19:15 +0800

    Create TestJava2_1.java
    将一个类声明为public，则也要将文件名称取成和这个类一样的名称，本例中文件名为TestJava2_1.java，而public之后所接的类名称也为TestJava2_1,
    也就是说，在一个java文件里，最多只有一个public类

---
+++ TestJava2_1.java
@@ -0,0 +1,12 @@
+//TestJava2_1.java,Java的简单范例
+public class TestJava2_1
+{
+public static void main(Sting args)
+  {
+  int num;//声明一个整形变量num
+  num=3;//将整形变量赋值为3
+  //输出字符串，这里用“+”号连接变量
+  System.out.println("这是数字"+num);
+  System.out.println("我有“+num+”本书！")；
+  }
+}
