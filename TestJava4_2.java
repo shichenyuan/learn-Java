065744925692cb10430e7d0c98f46c226f2cad1f
shichenyuan <34560365+shichenyuan@users.noreply.github.com>
2018-01-12 21:14:42 +0800

    Create TestJava4_2.java
    声明变量

---
+++ TestJava4_2.java
@@ -0,0 +1,11 @@
+//下面的程序声明了两个变量，一个整形，另一个是字符型
+public class TestJava4_2
+{
+  public static void main(Sting args[])
+  {
+    int num=3;//声明一整形变量num，赋值为3
+    char ch='z';//声明一字符变量ch，赋值为z
+    System.out.println(num+"是整数!");//输出num的值
+    System.out.println(ch+"是字符！");//输出ch的值
+  }
+}
