7b236280583b5a1c118bdf19c20dde59ed94e4f0
shichenyuan <34560365+shichenyuan@users.noreply.github.com>
2018-01-20 16:24:30 +0800

    Create TestJava5_2.java

---
+++ TestJava5_2.java
@@ -0,0 +1,9 @@
+//short类型数据的使用
+public class TestJava5_2
+{
+	public static void main(String args[])
+	{
+		short short_max=Short.MAX_VAL-UE;//得到短整型的最大值
+		System.out.println("SHORT的最大值："+short_max);
+	}
+}
