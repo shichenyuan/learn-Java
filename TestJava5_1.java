d7ad6da93ace99ddd542ad4860229312d5364abe
shichenyuan <34560365+shichenyuan@users.noreply.github.com>
2018-01-20 14:47:13 +0800

    Create TestJava5_1.java

---
+++ TestJava5_1.java
@@ -0,0 +1,9 @@
+//byte类型数据的使用
+public class TestJava5_1
+{
+  public static void main(String args[])
+  {
+    byte byte_max=Byte.MAX_VAL-UE;//得到Byte型的最大值
+    System.out.println("BYTE的最大值："+byte_max);
+  }
+}
